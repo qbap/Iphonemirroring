9eed97959ba726e48e855370c873b9692c52be44
qbap <jakubpawlak@me.com>
2024-09-20 22:15:22 +0200

    Update hazemirror.py

--- hazemirror.py
+++ hazemirror.py
@@ -1,44 +1,42 @@
 import os
 import subprocess
 import shutil
+from pathlib import Path
 
-# file paths
+# File paths
 system_file = "/private/var/db/os_eligibility/eligibility.plist"
-replacement_file = "/Users/YOUR_USERNAME/Downloads/hazemirror/eligibility.plist"  # change "YOUR_USERNAME" to your user,if u don't know your user type whoami command and take the user from there
+replacement_file = Path(__file__).parent / "eligibility.plist"  # automatically set replacement file to the same directory as this script
 
-
-# file change function
+# File replacement function
 def replace_files():
     try:
-        # backup create 
+        # Backup creation
         backup_file = f"{system_file}.backup"
         if not os.path.exists(backup_file):
             shutil.copy(system_file, backup_file)
+            print(f"Backup created at: {backup_file}")
 
-        
-        print("Root permisson required")
-        subprocess.run(['sudo', 'cp', replacement_file, system_file], check=True)
-        print("File changed successfully.")
+        print("Root permission required to replace system file.")
+        subprocess.run(['sudo', 'cp', str(replacement_file), system_file], check=True)
+        print("File replaced successfully.")
 
     except Exception as e:
         print(f"Error: {e}")
 
-
-# App start
+# Launch the iPhone Mirroring application
 def launch_mirroring_app():
     try:
         # Run with sudo
-        print("Application starting iPhone Mirroring...")
+        print("Starting iPhone Mirroring app...")
         subprocess.run(['open', '/System/Applications/iPhone Mirroring.app'], check=True)
-        print("Access granted.")
+        print("App launched successfully.")
 	
     except Exception as e:
-        print(f"Update os to macos sequoia,the error: {e}")
-
+        print(f"Error starting app. Consider updating to macOS Sequoia. The error: {e}")
 
 if __name__ == "__main__":
-    # file change
+    # Replace system file
     replace_files()
 
-    # app run
+    # Launch mirroring app
     launch_mirroring_app()
